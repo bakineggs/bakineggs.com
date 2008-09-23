45b54808117b62cb7dac112ef47b94bffe6829a7
Dan Barry <dan@chaptercommunications.com>
2008-09-23 00:58:19 -0500

    2 column page layout

--- shadow.js
+++ shadow.js
@@ -1,14 +1,26 @@
 $(document).ready(function() {
-  var cloneAndPlace = function(element, dom_class, offset) {
+  var place = function(element, target, offset) {
+    element.width(target.width()).height(target.height());
+    t_offset = target.offset();
+    element.css('top', t_offset['top']).css('left', t_offset['left']);
+    target.after(element);
     e_offset = element.offset();
-    clone = element.clone().addClass(dom_class).width(element.width()).height(element.height()).css('top', e_offset['top']).css('left', e_offset['left']);
-    element.after(clone);
-    c_offset = clone.offset();
-    clone.css('left', 2 * e_offset['left'] - c_offset['left'] + offset).css('top', 2 * e_offset['top'] - c_offset['top'] + offset);
+    element.css('left', 2 * t_offset['left'] - e_offset['left'] + offset).css('top', 2 * t_offset['top'] - e_offset['top'] + offset);
+  };
+
+  var cloneAndPlace = function(element, dom_class, offset) {
+    clone = element.clone().addClass(dom_class);
+    place(clone, element, offset);
   };
 
   var makeShadow = function(element) {
     element = $(element);
+    if (element.is('img')) {
+      place(element.clone().addClass('overShadow'), element, 0);
+      element.css('visibility', 'hidden');
+      place($(document.createElement('div')).addClass('blockShadow'), element, 3);
+      return;
+    }
     cloneAndPlace(element, 'shadow', 1);
     cloneAndPlace(element, 'overShadow', 0);
     element.css('visibility', 'hidden');
@@ -21,4 +33,5 @@ $(document).ready(function() {
   makeShadows('h1');
   makeShadows('h2');
   makeShadows('ul');
+  makeShadows('img');
 });
