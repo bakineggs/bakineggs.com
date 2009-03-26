b0bce58498f1061ad63cf508ee90bf7ae6040dcb
Dan Barry <dan@bakineggs.com>
2009-03-26 02:31:48 -0500

    implement recommendations from rss validator

--- pages/blog.rss.php
+++ pages/blog.rss.php
@@ -5,11 +5,13 @@ db_connect('bakineggs');
 require_once 'blog/entry.php';
 
 function render_document() {
+  header('Content-Type: application/rss+xml');
   echo '<?xml version="1.0"?>' . "\n";
-  echo '<rss version="2.0">' . "\n";
+  echo '<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">' . "\n";
   echo '  <channel>' . "\n";
   echo '    <title>bakineggs</title>' . "\n";
   echo '    <link>http://bakineggs.com/Blog</link>' . "\n";
+  echo '    <atom:link href="http://bakineggs.com/blog.rss" rel="self" type="application/rss+xml" />' . "\n";
   echo '    <description>Dan Barry\'s Blog</description>' . "\n";
   foreach (Entry::find_all() as $entry)
     render_entry($entry);
@@ -21,8 +23,9 @@ function render_entry($entry) {
   echo '    <item>' . "\n";
   echo '      <title>' . $entry->name . '</title>' . "\n";
   echo '      <link>http://bakineggs.com/Blog/' . urlencode($entry->name) . '</link>' . "\n";
+  echo '      <guid>http://bakineggs.com/Blog/' . urlencode($entry->name) . '</guid>' . "\n";
   echo '      <description>' . strip_tags($entry->summary) . '</description>' . "\n";
-  echo '      <pubDate>' . $entry->posted_at . '</pubDate>' . "\n";
+  echo '      <pubDate>' . date('r', $entry->posted_at) . '</pubDate>' . "\n";
   echo '    </item>' . "\n";
 }
 
