be35a93cb4062be167dde729f8432435cc674b83
Dan Barry <dan@bakineggs.com>
2009-03-26 02:14:07 -0500

    display summaries on blog listing

--- pages/blog.php
+++ pages/blog.php
@@ -9,22 +9,26 @@ function render_page($params) {
     render_entry(Entry::find_by_name(urldecode($params[0])));
   else
     foreach (Entry::find_all() as $entry)
-      render_entry($entry, true);
+      render_summary($entry, true);
 }
 
-function render_entry($entry, $link_name = false) {
+function render_entry($entry) {
   if (!$entry)
     return render_not_found();
   echo '<div class="entry" id="entry_' . h($entry->id) . '">' . "\n";
-  echo '<h3>';
-  if ($link_name)
-    echo '<a href="/Blog/' . urlencode($entry->name) . '">';
-  echo h($entry->name);
-  if ($link_name)
-    echo '</a>';
-  echo '</h3>' . "\n";
+  echo '<h3>' . h($entry->name) . '</h3>' . "\n";
   echo '<span class="posted_at">' . h(date('F j, Y @ g:ia', $entry->posted_at)) . '</span>' . "\n";
   echo '<p>' . $entry->body . '</p>' . "\n";
+  echo '</div>' . "\n";
+}
+
+function render_summary($entry) {
+  if (!$entry)
+    return;
+  echo '<div class="entry summary" id="entry_' . h($entry->id) . '">' . "\n";
+  echo '<h3><a href="/Blog/' . urlencode($entry->name) . '">' . h($entry->name) . '</a></h3>' . "\n";
+  echo '<span class="posted_at">' . h(date('F j, Y @ g:ia', $entry->posted_at)) . '</span>' . "\n";
+  echo '<p>' . $entry->summary . '</p>' . "\n";
   echo '</div>' . "\n";
 }
 
